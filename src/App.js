b0be56f9ddd88af7c6b77dcb0fdabac8d436309e
wildbear <hwsbear@gmail.com>
2022-03-03 15:00:21 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import './App.css';
-import logo from './logo.svg';
+import Customer from './components/Customer.js'
 
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'http://placeimg.com/64/64/tech/1', 
+  'name' : 'wildbear1'  ,
+  'birthday' : '1977/09/31'  ,
+  'gender' : '41'  ,
+  'job' : 'singer1'  ,
+  },
+  {
+  'id' : 2,
+  'image' : 'http://placeimg.com/64/64/tech/2', 
+  'name' : 'wildbear2'  ,
+  'birthday' : '1977/09/32'  ,
+  'gender' : '42'  ,
+  'job' : 'singer2'  ,
+  },
+  {
+    'id' : 3,
+    'image' : 'http://placeimg.com/64/64/tech/3', 
+    'name' : 'wildbear3'  ,
+    'birthday' : '1977/09/33'  ,
+    'gender' : '43'  ,
+    'job' : 'singer3'  ,
+   },
+   {
+     'id' : 4,
+     'image' : 'http://placeimg.com/64/64/tech/4', 
+     'name' : 'wildbear4'  ,
+     'birthday' : '1977/09/34'  ,
+     'gender' : '44'  ,
+     'job' : 'singer4'  ,
+    }
+]
 function App() {
   return (
-    <div className="gray-background">
-      Develop Management System!!!
+    <div>
+      {
+        customers.map( c => {
+          return (
+            <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday =  {c.birthday} gender =  {c.gender} job =  {c.job} />
+          )
+        })
+      }
+      
     </div>
   );
 }
