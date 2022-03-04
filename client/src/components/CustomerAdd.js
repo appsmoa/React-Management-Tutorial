2430c905c26fa3eeecb3892d5811ec7b39c41bf8
wildbear <hwsbear@gmail.com>
2022-03-04 17:29:33 +0900

    Add Material UI Design For Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,10 +1,20 @@
 import React, { Component, useState } from 'react';
 import {post} from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
 
+const styles = theme => ({
+    hidden : {
+        display:'none'
+    }
+})
 
 class CustomerAdd extends Component {
-
-    
 
     constructor (props){
         super(props);
@@ -15,13 +25,11 @@ class CustomerAdd extends Component {
             gender : '',
             job : '',
             fileName : '',
-            userNameError : ''
+            userNameError : '',
+            open : false
         }
         
     }
-
-   
-
 
     addCustomer = () =>{
         const url = '/api/customers';
@@ -41,7 +49,7 @@ class CustomerAdd extends Component {
 
     handleFormSubmit = (e) => {
         e.preventDefault();
-        if (!e.target.userName.value){
+        if (!this.state.userName){
             this.setState({
                 userNameError : 'err'
             })
@@ -59,9 +67,11 @@ class CustomerAdd extends Component {
                 gender : '',
                 job : '',
                 fileName : '',
-                userNameError : ''
+                userNameError : '',
+                open : false
             })
         }
+
     }
 
     handleFileChange = (e) =>{
@@ -82,22 +92,67 @@ class CustomerAdd extends Component {
         this.setState(nextState)
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open:true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file : null,
+            userName : '',
+            birthday : '',
+            gender : '',
+            job : '',
+            fileName : '',
+            userNameError : '',
+            open : false
+        });
+    }
+
     render() {
-         
+        const {classes} = this.props;
         return (
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>고객추가</h1>
-                Image : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
-                name : <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/>
-                {this.state.userNameError && <span style={{color : 'red',fontSize:'0.8em'}}> Name은 필수 항목입니다.</span>}
-                <br/>
-                birthday : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                gender : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                job : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                <button type="submit">추가하기</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    Customer Add
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>Customer Add</DialogTitle>
+                        <DialogContent>
+                            <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} />
+                            <label htmlFor="raised-button-file">
+                                <Button variant="contained" color="primary" component="span" name="file">
+                                    {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                                </Button>
+                            </label>
+                            <br/>
+                            <TextField label="name" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+                            {this.state.userNameError && <span style={{color : 'red',fontSize:'0.8em'}}> Name은 필수 항목입니다.<br/></span>}
+                            <TextField label="birthday" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                            <TextField label="gender" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                            <TextField label="job" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>Add</Button> 
+                            <Button variant="outlined" color="primary" onClick={this.handleClose}>Close</Button>                          
+                        </DialogActions>
+                </Dialog>
+            </div>
+            // <form onSubmit={this.handleFormSubmit}>
+            //     <h1>고객추가</h1>
+            //     Image : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+            //     name : <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/>
+            //     {this.state.userNameError && <span style={{color : 'red',fontSize:'0.8em'}}> Name은 필수 항목입니다.</span>}
+            //     <br/>
+            //     birthday : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+            //     gender : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+            //     job : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+            //     <button type="submit">추가하기</button>
+            // </form>
         );
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class CustomerDelete extends Component {
+
+    constructor (props){
+        super(props);
+        this.state = {
+            open : false
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open:true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open : false
+        });
+    }
 
     deletedCustomer(id){
         const url = '/api/customers/' + id;
@@ -12,7 +37,21 @@ class CustomerDelete extends Component {
 
     render() {
         return (
-            <button onClick={(e)=>{ this.deletedCustomer(this.props.id) }}>삭제</button>
+            <div>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>삭제경고</DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            정말 삭제 하시겠습니까?                            
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={(e) => this.deletedCustomer(this.props.id)}>Del</Button> 
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>Close</Button>                          
+                    </DialogActions>
+                </Dialog>
+            </div>
         );
     }
 }
